6b8826c3aeec80d45a93748ffe77780ef7032325
gamemaker230 <149206812+gamemaker230@users.noreply.github.com>
2023-10-27 17:38:22 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,10 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
-}
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My HTML Page</title>
+  </head>
+  <body>
+    <h1>My HTML Page</h1>
+    <p>This is my HTML page.</p>
+  </body>
+</html>
